ba6866c28cc8bac90d87d975dd8135823dbd5f72
kzitouni <zkarim091019@gmail.com>
2021-02-23 11:38:05 -0500

    resume link

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -13,7 +13,7 @@ export default function Hero(props){
           <Title>I am <Name>Karim Zitouni</Name><br />I&#x27;m a Front End Developer</Title>
           <Paragraph>I specialize in developing exceptionally modern websites, applications and everything in between!</Paragraph>
           <ButtonsContainer>
-            <OutlineButton href="http://bit.ly/karimresume" target="_blank">View Resume</OutlineButton>
+            <OutlineButton href="http://bit.ly/karim-resume" target="_blank">View Resume</OutlineButton>
             <FillButton href="mailto: karimzitouni05@gmail.com">Hire me!</FillButton>
           </ButtonsContainer>
           <SocialText>Check out my Github:</SocialText>
